6c7c254324adcf3d5eb8e13ea91bd3498f3c9dbe
guilherme.varussa <guilherme.varussa@sflabs.com.br>
2022-09-10 10:18:26 -0400

    fixed name requisition

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ server.use(express.json());
 
 const fishies = ['tilapia', 'pintado', 'tambatinga', 'jatuarana'];
 
-//get a fish
 server.get('/fishies/:index', (req, resp) => {
     const { index } = req.params
 
@@ -14,37 +13,30 @@ server.get('/fishies/:index', (req, resp) => {
 });
 
 
-//get all fishies
 
 server.get('/fishies', (req, resp) => {
     return resp.json(fishies)
 });
 
 
-//create 
-
 server.post('/fishies', (req, resp) => {
-    const name = req.body
+    const { name } = req.body
     fishies.push(name);
 
     return resp.json(fishies)
 
 });
 
-//update fish
 
 server.put('/fishies/:index', (req, resp) => {
     const { index } = req.params
     const { name } = req.body
 
-    fishies[index] = name
+    fishies[index] = name;
 
     return resp.json(fishies);
 
 });
-
-
-//delete
 
 server.delete('/fishies/:index', (req, resp) => {
     const { index } = req.params
@@ -53,7 +45,7 @@ server.delete('/fishies/:index', (req, resp) => {
     fishies.splice(index, 1)
 
     return resp.json({ message: 'the fish was deleted' })
-})
+});
 
 
 
