4fee987c41a827a90d530dfb140a2b3d929188fe
guilherme.varussa <guilherme.varussa@sflabs.com.br>
2022-09-10 08:24:18 -0400

    create gets and post to fishies

--- index.js
+++ index.js
@@ -9,15 +9,27 @@ const fishies = ['tilapia', 'pintado', 'tambatinga', 'jatuarana'];
 //get a fish
 server.get('/fishies/:index', (req, resp) => {
     const { index } = req.params
+
+    return resp.json(fishies[index]);
+});
+
+
+//get all fishies
+
+server.get('/fishies', (req, resp) => {
+    return resp.json(fishies)
+});
+
+
+//create 
+
+server.post('/fishies', (req, resp) => {
+    const name = req.body
+    fishies.push(name);
+
+    return resp.json(fishies)
+
 })
-
-
-
-
-
-
-
-
 
 
 
